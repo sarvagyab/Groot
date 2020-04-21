3900f29c16affd58bd236bc1f6f4aa756dda8418
Sarvagya Bansal <sarvagya60@gmail.com>
2020-04-22 04:46:28 +0530

    Minor updates - - Refactored Code - Added GUI tip for arrows in self.treeWidget (issue: black arrows on black background not visible)

--- Application/modules/noteHandling.py
+++ Application/modules/noteHandling.py
@@ -3,9 +3,6 @@ from PySide2 import QtCore, QtWidgets
 import json
 
 def loadNote(path, _textEdit):
-    # file = QtCore.QFile(path)
-    # file.open(QtCore.QIODevice.Text | QtCore.QIODevice.ReadWrite)
-    # stream = QtCore.QTextStream(file)
     _textEdit.setPlainText(readText(path))
     QtWidgets.QApplication.processEvents()
 
