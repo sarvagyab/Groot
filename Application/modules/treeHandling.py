1debdf3cf636509656cc82bfb7c28938395ed5c3
Sarvagya Bansal <sarvagya60@gmail.com>
2020-04-25 00:49:03 +0530

    Changed File Structure
    - changed the way notes and notebooks are represented in fileStructure.json
    - adjusted the loadFileStructure(treeHandling.py) and noteLoader(treeHandling) functions to use this fileStructure

--- Application/modules/treeHandling.py
+++ Application/modules/treeHandling.py
@@ -19,11 +19,11 @@ def fixTreeViewScrolling(tree):
 def fillItem(item,valDict):
     for key,val in valDict.items():
         newItem = QtWidgets.QTreeWidgetItem()
-        newItem.setText(0,key)
-        if "path" in val and type(val["path"]) == str:
+        newItem.setText(0,val["name"])
+        if "path" in val["expanded"] and type(val["expanded"]["path"]) == str:
             pass
         else :
-            fillItem(newItem,val)
+            fillItem(newItem,val["expanded"])
 
         item.addChild(newItem)
 
@@ -38,17 +38,26 @@ def loadfileStructure(tree):
     fillItem(item, structDict["Uncategorized"])
 
 
-def _itemVal(item,noteTree):
-    if (item.text(0)!="Notebooks" and item.text(0)!= "Uncategorized"):
-        return _itemVal(item.parent(),noteTree)[item.text(0)]
+def getLineage(item):
+    if item.parent() is None:
+        return [item.text(0)]
+    else:
+        return getLineage(item.parent()) + [item.parent().indexOfChild(item)]
 
-    return noteTree[item.text(0)]
-
+def _itemVal(pathList,noteTree):
+    if pathList == []:
+        return []
+    for item in pathList:
+        keys = [*noteTree]
+        noteTree = noteTree[keys[item]]["expanded"]
+    return noteTree
 
 # returns the value associated with an item in treeWidget
 def itemVal(item):
     structDict = getJsonTree()
-    return _itemVal(item,structDict)
+    pathList = getLineage(item)
+    structDict = structDict[pathList[0]]
+    return _itemVal(pathList[1:],structDict)
 
 def _updateItem(changeDict,structDict): # DFS
     if(type(structDict) == type({})): 
