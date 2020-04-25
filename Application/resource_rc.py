bcce5049dd8f8661ebbba5eef2104f05bde72660
Sarvagya Bansal <sarvagya60@gmail.com>
2020-04-26 02:37:46 +0530

    Changes resource_rc to use PySide2

--- Application/resource_rc.py
+++ Application/resource_rc.py
@@ -1,12 +1,5 @@
-# -*- coding: utf-8 -*-
 
-# Resource object code
-#
-# Created by: The Resource Compiler for PyQt5 (Qt v5.14.1)
-#
-# WARNING! All changes made in this file will be lost!
-
-from PyQt5 import QtCore
+from PySide2 import QtCore
 
 qt_resource_data = b"\
 \x00\x00\x17\x50\
