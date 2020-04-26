d7b63dde3e423f2925bcd168fe95f8479d3e4101
Sarvagya Bansal <sarvagya60@gmail.com>
2020-04-26 06:52:56 +0530

    Changed itemVal structure for easier editing

--- Application/modules/treeHandling.py
+++ Application/modules/treeHandling.py
@@ -49,27 +49,29 @@ def getLineage(item):
 
 
 def _itemVal(pathList,noteTree):
-    if pathList == []:
-        return []
+    keyindex = pathList.pop()
     for item in pathList:
         keys = [*noteTree]
         noteTree = noteTree[keys[item]]["expanded"]
-    return noteTree
+    key  = [*noteTree][keyindex]
+    return [key,noteTree]
 
 
 # returns the value associated with an item in treeWidget
 def itemVal(item):
     structDict = getJsonTree()
     pathList = getLineage(item)
-    structDict = structDict[pathList[0]]
-    return _itemVal(pathList[1:],structDict)
+    if len(pathList) == 1:
+        return [pathList[0],structDict,structDict]
+    return _itemVal(pathList[1:],structDict[pathList[0]]) + [structDict]
 
 
 def isNote(item):
     details = itemVal(item)
-    if("path" in details and type(details["path"]) == str):
-        return [True,details]
-    return [False,details]
+    details = details[1][details[0]]
+    if("expanded" in details and "path" in details["expanded"] and type(details["expanded"]["path"]) == str):
+        return [True,details["expanded"]]
+    return [False,[]]
 
 
 def _updateItem(changeDict,structDict): # DFS
