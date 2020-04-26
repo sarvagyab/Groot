8e2606715d12bc44299a0b21622cb41aaa3bb975
hitanshu-mehta <hitanshu99amehta@gmail.com>
2020-04-26 10:56:58 +0530

    Some changes in search module

--- Application/modules/searchInNote.py
+++ Application/modules/searchInNote.py
@@ -4,6 +4,7 @@ from PySide2.QtCore import QRegExp,Qt
 def searchText(ui,cursor = QTextCursor.Start,reversed = False):
     ui.errorLabel.setText("")
     ui.plainTextEdit.moveCursor(cursor) # set the cursor at the beginning of the text
+    ui.mdViewer.moveCursor(cursor)
     search_text = ui.searchBar.text()
     matchWholeWord = ui.wholeWord.isChecked()
     matchregex = ui.regexButton.isChecked()
@@ -16,12 +17,15 @@ def searchText(ui,cursor = QTextCursor.Start,reversed = False):
                 if(reversed == True):
                     mode = mode | QTextDocument.FindBackward
                 found = ui.plainTextEdit.find(search_regex,mode)
+                ui.mdViewer.find(search_regex,mode)
             else:
                 if(reversed == True):
                     mode = QTextDocument.FindBackward
                     found = ui.plainTextEdit.find(search_regex,mode)
+                    ui.mdViewer.find(search_regex,mode)
                 else:    
                     found = ui.plainTextEdit.find(search_regex)
+                    ui.mdViewer.find(search_regex)
         else:
             search_regex = QRegExp(search_text,Qt.CaseInsensitive)
             if(matchWholeWord == True):
@@ -29,12 +33,15 @@ def searchText(ui,cursor = QTextCursor.Start,reversed = False):
                 if(reversed == True):
                     mode = mode | QTextDocument.FindBackward
                 found = ui.plainTextEdit.find(search_regex,mode)
+                ui.mdViewer.find(search_regex,mode)
             else:
                 if(reversed == True):
                     mode = QTextDocument.FindBackward
                     found = ui.plainTextEdit.find(search_regex,mode)
+                    ui.mdViewer.find(search_regex,mode)
                 else:    
                     found = ui.plainTextEdit.find(search_regex)
+                    ui.mdViewer.find(search_regex)
     else:
         if(matchCase == True):
             mode = QTextDocument.FindCaseSensitively
@@ -43,17 +50,21 @@ def searchText(ui,cursor = QTextCursor.Start,reversed = False):
             if(matchWholeWord == True):
                 mode = mode |QTextDocument.FindWholeWords
                 found = ui.plainTextEdit.find(search_text,mode)
+                ui.mdViewer.find(search_text,mode)
         else:
             if(matchWholeWord == True):
                 mode = QTextDocument.FindWholeWords
                 if(reversed == True):
                     mode = mode | QTextDocument.FindBackward
                 found = ui.plainTextEdit.find(search_text,mode)
+                ui.mdViewer.find(search_text,mode)
             else:
                 if(reversed == True):
                     found = ui.plainTextEdit.find(search_text,QTextDocument.FindBackward)
+                    ui.mdViewer.find(search_text,QTextDocument.FindBackward)
                 else:
                     found = ui.plainTextEdit.find(search_text)
+                    ui.mdViewer.find(search_text)
     if(found == False):
         MSG = "<html><head/><body><p><span style=\" color:#ff0000;\">No results</span></p></body></html>"
         ui.errorLabel.setText(MSG)
